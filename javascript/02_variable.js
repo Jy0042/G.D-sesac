eebb11e084794d5bf22f9197127824c63700981c
jy0042 <sjy6630@gmail.com>
2024-02-21 17:00:12 +0900

    24.02.21 bootstrap and  javascript study

---
+++ javascript/02_variable.js
@@ -0,0 +1,43 @@
+console.log("connected");
+
+// 변수 선언
+var varName;
+
+// 변수 할당
+varName = "James";
+
+// 변수 선언과 할당을 동시에
+var varName = "James";
+
+// 변수 재할당
+varName = "Tom";
+
+var varName = "Jane";
+
+console.log(varName);
+
+aa = 123;
+
+console.log(aa);
+
+
+/* let 키워드
+    1. 변수 선언 -> 할당
+    2. 변수 선언과 값 할당을 동시에
+*/
+
+//변수 선언
+let letName;
+
+//값 할당
+letName = "사과";
+console.log(letName);
+
+//변수 선언과 값 할다을 동시에
+let letName2 = "망고";
+console.log(letName2);
+
+// let letName2 = "바나나"; // error: 중복 선언 불가
+
+letName2 = "바나나"; // 값을 재할당
+console.log(letName2);
