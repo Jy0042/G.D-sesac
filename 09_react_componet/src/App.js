7305431a0328c7ef7b59984c49459bc7b839674e
jy0042 <sjy6630@gmail.com>
2024-04-03 16:58:50 +0900

    24.04.03 js study : creact-react-app, component

---
+++ 09_react_componet/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ClassComponent from "./components/ClassComponent.js";
+import FunctionComponent from "./components/FunctionComponent.js";
+
+
+function App() {
+  return (
+    <div className="App">
+      <ClassComponent />
+      <FunctionComponent />
+    </div>
+  );
+}
+
+export default App;
